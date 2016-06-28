013100136e43a3afcabfd335f43ebb06051a1eea
Jianbo Ye <yelpoo@gmail.com>
2016-06-28 14:53:13 +0200

    some enhancement

--- cxx/knn_script.py
+++ cxx/knn_script.py
@@ -11,9 +11,13 @@ train = raw_data[:,0:(raw_data.shape[1]-1)]
 label = raw_data[:,(raw_data.shape[1]-1)]
 del raw_data
 nbrs = NearestNeighbors(n_neighbors=k, algorithm='kd_tree').fit(train)
-test = np.loadtxt('ffm_te_fact.txt');
+
+raw_data = np.loadtxt('ffm_te_knn_data.txt');
+test = raw_data[:,0:(raw_data.shape[1]-1)]
+test_label= raw_data[:,(raw_data.shape[1]-1)]
 size=test.shape[0];
 distances, indices = nbrs.kneighbors(test)
 knn_est = np.mean(np.reshape(label[indices.ravel()], indices.shape), 1)
 distances = np.mean(distances, 1)
-np.savetxt('knn_te_pred.txt', np.concatenate(knn_est,distances).reshape((2,size)).T)
+print "rmse =", np.sqrt(np.mean((test_label - knn_est)**2))
+np.savetxt('knn_te_pred.txt', np.concatenate((knn_est,distances)).reshape((2,size)).T)
