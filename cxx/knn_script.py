8c312deab3c89446fa5d24eea7b35193c25d2b23
Jianbo Ye <yelpoo@gmail.com>
2016-06-30 11:23:01 +0200

    Revert "use flann"
    This reverts commit 8726e2c65b28b6dec53324bb3d5e5c0ce1e101f2.

--- cxx/knn_script.py
+++ cxx/knn_script.py
@@ -1,4 +1,4 @@
-from pyflann import *
+from sklearn.neighbors import NearestNeighbors
 import numpy as np
 
 
@@ -9,7 +9,7 @@ raw_data = np.loadtxt('ffm_tr_knn_data.txt')
 train = raw_data[:,0:(raw_data.shape[1]-1)]
 label = raw_data[:,(raw_data.shape[1]-1)]
 del raw_data
-flann = FLANN()
+nbrs = NearestNeighbors(n_neighbors=k, algorithm='kd_tree').fit(train)
 
 raw_data_txt = np.loadtxt('ffm_te_knn_data.txt', delimiter='\n', dtype=str);
 raw_data = -999 * np.ones((len(raw_data_txt), d+1))
@@ -24,7 +24,7 @@ test_label= raw_data[:,(raw_data.shape[1]-1)]
 del raw_data
 
 size=test.shape[0];
-indices, distances = flann.nn(train, test, k, algorithm="kmeans")
+distances, indices = nbrs.kneighbors(test)
 knn_est = np.mean(np.reshape(label[indices.ravel()], indices.shape), 1)
 knn_est[test_label == -999] = -999
 distances = np.mean(distances, 1)
