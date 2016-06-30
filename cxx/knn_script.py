8726e2c65b28b6dec53324bb3d5e5c0ce1e101f2
Jianbo Ye <yelpoo@gmail.com>
2016-06-30 02:59:08 +0200

    use flann

--- cxx/knn_script.py
+++ cxx/knn_script.py
@@ -1,4 +1,4 @@
-from sklearn.neighbors import NearestNeighbors
+from pyflann import *
 import numpy as np
 
 
@@ -9,7 +9,7 @@ raw_data = np.loadtxt('ffm_tr_knn_data.txt')
 train = raw_data[:,0:(raw_data.shape[1]-1)]
 label = raw_data[:,(raw_data.shape[1]-1)]
 del raw_data
-nbrs = NearestNeighbors(n_neighbors=k, algorithm='kd_tree').fit(train)
+flann = FLANN()
 
 raw_data_txt = np.loadtxt('ffm_te_knn_data.txt', delimiter='\n', dtype=str);
 raw_data = -999 * np.ones((len(raw_data_txt), d+1))
@@ -24,7 +24,7 @@ test_label= raw_data[:,(raw_data.shape[1]-1)]
 del raw_data
 
 size=test.shape[0];
-distances, indices = nbrs.kneighbors(test)
+indices, distances = flann.nn(train, test, k, algorithm="kmeans")
 knn_est = np.mean(np.reshape(label[indices.ravel()], indices.shape), 1)
 knn_est[test_label == -999] = -999
 distances = np.mean(distances, 1)
